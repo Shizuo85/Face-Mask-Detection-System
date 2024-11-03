4196fd2596c016592b3b154919777d837264d3b4
Ekene <ekeneanthony85@gmail.com>
2024-11-03 05:59:04 +0100

    first commit

---
+++ ESP32_Mask_final/sketch_aug18a/sketch_aug18a.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(33, OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(33, LOW);
+  delay(1000);
+  digitalWrite(33, HIGH);
+  delay(1000);
+  // put your main code here, to run repeatedly:
+
+}
